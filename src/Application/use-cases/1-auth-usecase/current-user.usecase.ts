cdbdd1419b0380015e7f7ae88692349a440d7cf0
Nithin Joji0756 <nithinjoji0756@gmail.com>
2025-01-20 02:30:15 +0530

    update

--- src/Application/use-cases/1-auth-usecase/current-user.usecase.ts
+++ src/Application/use-cases/1-auth-usecase/current-user.usecase.ts
@@ -1,9 +1,10 @@
+import { IUseCase } from "shared/IUsecase";
 import { User } from "../../../Domain/Entities/User";
 
 import { UserRepository } from "../../../Infrastructure/databse/mongoose/Repositories/user.respository";
 import { BadRequestError } from "../../../Presentation/error/error.interface";
 import { IRepoResponse } from "../../../shared/IBase-repository";
-import { IUseCase } from "../../../shared/IUsecase";
+// import { IUseCase } from "../../../shared/IUsecase";/
 
 
 
